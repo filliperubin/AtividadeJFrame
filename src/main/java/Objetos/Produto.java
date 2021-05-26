ecda29432b915f81f2e88473c63ea2bc2b239534
Fillipe <Fillipe@192.168.15.7>
2021-05-26 16:46:39 -0300

    Realizada Atividade Proposta em Aula

---
+++ src/main/java/Objetos/Produto.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Fillipe
+ */
+public class Produto {
+    private String nome;
+    private String descricao;
+    private double valorUnit;
+    private int quantidade;
+    private double valorTotal;
+    private String fornecedor;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the valorUnit
+     */
+    public double getValorUnit() {
+        return valorUnit;
+    }
+
+    /**
+     * @param valorUnit the valorUnit to set
+     */
+    public void setValorUnit(double valorUnit) {
+        this.valorUnit = valorUnit;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valorTotal
+     */
+    public double getValorTotal() {
+        return valorTotal = quantidade * valorUnit;
+        
+    }
+
+    /**
+     * @param valorTotal the valorTotal to set
+     */
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+       
+    }
+
+    /**
+     * @return the fornecedor
+     */
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    /**
+     * @param fornecedor the fornecedor to set
+     */
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+    
+}
